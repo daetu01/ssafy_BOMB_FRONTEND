c0d4066a5819db292ef459b92a084878b8bbdad6
daetu01 <daetu01@gachon.ac.kr>
2025-11-28 00:29:58 +0900

    first init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+
+const bombs = ref("")
+
+onMounted(() => {
+  axios.get('/api/menu')
+    .then(res => {
+      bombs.value = res.data
+    })
+    .catch(err => {
+      console.error(err)
+    })
+})
+</script>
+
+<template>
+  <div>
+    <h1>Bomb 데이터</h1>
+    <ul>
+      <li>
+        {{ bomb }}
+      </li>
+    </ul>
+  </div>
+</template>
