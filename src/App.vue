517ca1f8a01c84233b5962c9d81546696b6a9bd1
daetu01 <daetu01@gachon.ac.kr>
2025-11-28 00:51:45 +0900

    fix the connection to spring boot "

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ const bombs = ref("")
 onMounted(() => {
   axios.get('/api/menu')
     .then(res => {
+      console.log(res.data)
       bombs.value = res.data
     })
     .catch(err => {
@@ -20,7 +21,7 @@ onMounted(() => {
     <h1>Bomb 데이터</h1>
     <ul>
       <li>
-        {{ bomb }}
+        {{ bombs }}
       </li>
     </ul>
   </div>
